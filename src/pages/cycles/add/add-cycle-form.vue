b8fa935ca436b57e818c22feb0fd75dd1aa7c876
Jose <jmgayosso@mightywizards.com>
2020-02-24 18:05:44 -0600

    Start form

--- src/pages/cycles/add/add-cycle-form.vue
+++ src/pages/cycles/add/add-cycle-form.vue
@@ -2,34 +2,38 @@
     .template-form
         q-form.q-ma-md.q-gutter-y-md
             q-input(filled
-                v-model="name"
-                label="Your name *"
-                hint="Name and surname"
+                v-model="circulating_seeds"
+                label="circulating_seeds"
+                hint="circulating_seeds"
                 lazy-rules
+                type="number"
+                :rules="[ val => val && val.length > 0 || 'Please type something']")
+            q-input(filled
+                v-model="volume_growth"
+                label="volume_growth"
+                hint="volume_growth"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Please type something']")
+            q-input(filled
+                v-model="seeds_destroyed"
+                label="seeds_destroyed"
+                hint="seeds_destroyed"
+                lazy-rules
+                type="number"
                 :rules="[ val => val && val.length > 0 || 'Please type something']")
             q-input(filled
                 v-model="name"
                 label="Your name *"
                 hint="Name and surname"
                 lazy-rules
+                type="number"
                 :rules="[ val => val && val.length > 0 || 'Please type something']")
             q-input(filled
                 v-model="name"
                 label="Your name *"
                 hint="Name and surname"
                 lazy-rules
-                :rules="[ val => val && val.length > 0 || 'Please type something']")
-            q-input(filled
-                v-model="name"
-                label="Your name *"
-                hint="Name and surname"
-                lazy-rules
-                :rules="[ val => val && val.length > 0 || 'Please type something']")
-            q-input(filled
-                v-model="name"
-                label="Your name *"
-                hint="Name and surname"
-                lazy-rules
+                type="number"
                 :rules="[ val => val && val.length > 0 || 'Please type something']")
             q-input(filled
                 v-model="name"
@@ -105,7 +109,10 @@ export default {
   name: 'add-cycle-form',
   data () {
     return {
-      name: ''
+      name: '',
+      circulating_seeds: '',
+      volume_growth: '',
+      seeds_destroyed: ''
     }
   }
 }
